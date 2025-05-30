b50352642260d7493515c278a7d3ca0b82ca7e5b
ShaphanHawks <shaphan@gmail.com>
2025-05-30 16:43:02 -0600

    admin

---
+++ app/admin/test/page.tsx
@@ -0,0 +1,8 @@
+export default function TestPage() {
+  return (
+    <div className="p-4">
+      <h1>Admin Test Page</h1>
+      <p>If you can see this, the admin routes are working.</p>
+    </div>
+  )
+} 
